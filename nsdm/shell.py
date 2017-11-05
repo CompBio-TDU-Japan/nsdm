61195463e947d3c78bd94279c117176da136aa68
Yuto Kimura <biostudy28@gmail.com>
2017-11-06 01:33:56 +0900

    test

--- nsdm/shell.py
+++ nsdm/shell.py
@@ -8,11 +8,11 @@ def run(command):
     if isinstance(command, str):
         string = command.replace("\n", " ")
         command = ["bash", "-c"] + [string]
-
     elif isinstance(command, list):
         pass
     else:
         return "invalid input:" + str(command), False
+
     pout = subprocess.run(command,
                           stdout=subprocess.PIPE,
                           stderr=subprocess.PIPE,
@@ -22,7 +22,8 @@ def run(command):
         returnstr = "#[info] Error! This command has stopped working.(see below)\n"
         returnstr += pout.stderr.decode("UTF-8").strip()
         returnstr += "#" + str(command)
-        return returnstr, False
+        return "", returnstr
     else:
-        returnstr = pout.stdout.decode("UTF-8").strip()
-        return returnstr, True
+        stdoutstr = pout.stdout.decode("UTF-8").strip()
+        stderrstr = pout.stdout.decode("UTF-8").strip()
+        return stdoutstr, stderrstr
