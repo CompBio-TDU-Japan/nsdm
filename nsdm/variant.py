90d2cce404f8c38cba6ea34a152b46e982492128
Yuto Kimura <biostudy28@gmail.com>
2017-11-08 16:38:18 +0900

    test

--- nsdm/variant.py
+++ nsdm/variant.py
@@ -12,6 +12,13 @@ def allcheck(ref_file, vcfdir, gff_file):
     variants = fileparse.vcf_allread(vcfdir)
     summary = fileparse.v_union(variants, gff_dict)
 
+    header = "\t".join([
+        "gene",
+        "ref",
+        "alt",
+        "pos_Pseq",
+    ])
+    print(header)
     for k, v in summary.items():
         reference.variant = v
         variantlist = reference.provean()
