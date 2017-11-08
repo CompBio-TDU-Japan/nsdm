c03d9dd3f004be32b13cd134cf47e163cb1b35ae
Yuto Kimura <biostudy28@gmail.com>
2017-11-08 13:40:41 +0900

    test

--- nsdm/seq.py
+++ nsdm/seq.py
@@ -58,7 +58,8 @@ class Ref:
                 v.pvp = math.ceil((v.nvp + 1) / 3) - 1
             result.append(v)
         vseq = "".join(vnseq)[start:end]
-        [print(x.__dict__) for x in self.variant]
+        [print(x.__dict__)
+         for x in self.variant if x.annotation == "missense_variant"]
         [print(vseq[x.nvp]) for x in result]
         if len(result) == 0:
             return (seq.split("*")[0], result)
