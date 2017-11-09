a929575d14eacd0f6facd1a30a73fce9f6a3c7dc
Yuto Kimura <biostudy28@gmail.com>
2017-11-09 14:58:44 +0900

    test

--- nsdm/variant.py
+++ nsdm/variant.py
@@ -11,34 +11,39 @@ def allcheck(ref_file, vcfdir, gff_file):
     header = "\t".join([
         "gene",
         "aa_change",
+        "impact",
     ])
     print(header)
     for k, variantlist in summary.items():
         for v in variantlist:
-            if v.info["SNPEFF_FUNCTIONAL_CLASS"] != "MISSENSE":
+            if v.info["SNPEFF_EFFECT"] != "NON_SYNONYMOUS_CODING":
                 continue
             aach = v.info["SNPEFF_AMINO_ACID_CHANGE"]
             payload = "\t".join([
                 v.gene,
                 aach,
+                v.impact,
             ])
-            if aach[0] == aach[-1]:
-                print("#same!" + payload)
+            if v.impact == "MISSENSE":
+                if aach[0] == aach[-1]:
+                    print("#same!" + payload)
+                else:
+                    print(payload)
             else:
                 print(payload)
-                print(payload, v.info["SNPEFF_EFFECT"])
-#    for k, v in summary.items():
-#        reference.variant = v
-#        variantlist = reference.provean()
-#        if len(variantlist) > 0:
-#            for vinfo in variantlist:
-#                if vinfo.pref == vinfo.palt:
-#                    print("\t".join([
-#                        vinfo.gene,
-#                        str(vinfo.pvp),
-#                        vinfo.pref,
-#                        vinfo.palt,
-#                        str(vinfo.pos),
-#                        str(vinfo.ref),
-#                        str(vinfo.alt),
-#                    ]))
+
+                #    for k, v in summary.items():
+                #        reference.variant = v
+                #        variantlist = reference.provean()
+                #        if len(variantlist) > 0:
+                #            for vinfo in variantlist:
+                #                if vinfo.pref == vinfo.palt:
+                #                    print("\t".join([
+                #                        vinfo.gene,
+                #                        str(vinfo.pvp),
+                #                        vinfo.pref,
+                #                        vinfo.palt,
+                #                        str(vinfo.pos),
+                #                        str(vinfo.ref),
+                #                        str(vinfo.alt),
+                #                    ]))
