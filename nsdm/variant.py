9af73fc6a55d24d0f4d8e3cbaadc4b9ce436c06e
Yuto Kimura <biostudy28@gmail.com>
2017-11-09 18:18:46 +0900

    test

--- nsdm/variant.py
+++ nsdm/variant.py
@@ -8,46 +8,16 @@ def allcheck(ref_file, vcfdir, gff_file):
     variants = fileparse.vcf_allread(vcfdir)
     summary = fileparse.v_union(variants, gff_dict)
 
-    header = "\t".join([
-        "gene",
-        "aa_change",
-        "impact",
-        "annotation",
-        "description",
-    ])
-    print(header)
+    result = dict()
     for k, variantlist in summary.items():
+        description = variantlist[0].description
         for v in variantlist:
             if v.info["SNPEFF_EFFECT"] == "SYNONYMOUS_CODING":
                 continue
             aach = v.info["SNPEFF_AMINO_ACID_CHANGE"]
-            payload = "\t".join([
-                v.gene,
+            payload = "|".join([
                 aach,
                 v.annotation,
-                v.impact,
-                v.description,
             ])
-            if v.annotation == "MISSENSE":
-                if aach[0] == aach[-1]:
-                    print("#same!" + payload)
-                else:
-                    print(payload)
-            else:
-                print(payload)
-
-                #    for k, v in summary.items():
-                #        reference.variant = v
-                #        variantlist = reference.provean()
-                #        if len(variantlist) > 0:
-                #            for vinfo in variantlist:
-                #                if vinfo.pref == vinfo.palt:
-                #                    print("\t".join([
-                #                        vinfo.gene,
-                #                        str(vinfo.pvp),
-                #                        vinfo.pref,
-                #                        vinfo.palt,
-                #                        str(vinfo.pos),
-                #                        str(vinfo.ref),
-                #                        str(vinfo.alt),
-                #                    ]))
+            result[k] = result.get(k, []) + payload
+        print(k + "\t" + result[k] + "\t" + description)
