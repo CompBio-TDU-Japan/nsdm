0f5f9e2da81b73dd63a39b9a6f2c0a470c9cc844
Yuto Kimura <biostudy28@gmail.com>
2017-11-15 12:47:16 +0900

    test

--- nsdm/fileparse.py
+++ nsdm/fileparse.py
@@ -133,6 +133,20 @@ def gff_read(filename):
     return result
 
 
+def gff_add(variants, gffdict=None):
+    vsum = dict()
+    for v in variants:
+        print(v)
+#        if in_cobj(vsum.get(v.gene, []), v) == False:
+#            gff = gffdict[v.gene]
+#            v.start = gff.start
+#            v.end = gff.end
+#            v.strand = gff.strand
+#            v.description = gff.description
+#            vsum[v.gene] = vsum.get(v.gene, []) + [v]
+#    return vsum
+
+
 def v_intersect(variant_list):
     vsum = dict()
     for g in variant_list:
